9ff5d8408d56dbbb9098795dc0bf1e8406240b81
isaac ikusika <isaacikusika@gmail.com>
2020-11-21 13:59:01 +0100

    added a new component where admin products are created

--- src/components/admin/products-form.jsx
+++ src/components/admin/products-form.jsx
@@ -1,13 +1,14 @@
 import {React} from 'react'
-import {withRouter} from 'react-router-dom'
+import {Redirect, withRouter} from 'react-router-dom'
 
 function ProductForm(props)
 {
     if (props.user.isAdmin){
 
     return ( <h4>Products Form works</h4> );
-}
-    <h1>no user</h1>
+    }
+    return <Redirect to="/home" />
+    
 }
  
 export default ProductForm;
