a8e855447690c0b585d279085a0c9791ccc512a8
isaac ikusika <isaacikusika@gmail.com>
2020-12-02 09:07:28 +0100

    implementing search on product list page

--- src/components/admin/admin-products.jsx
+++ src/components/admin/admin-products.jsx
@@ -1,17 +1,17 @@
 import {React, useState, useEffect} from 'react'
-import {Button, Table} from 'react-bootstrap'
+import {Button, Table, Form} from 'react-bootstrap'
 import {Redirect, useHistory, Link} from 'react-router-dom'
 import Products from '../../Data/Products.json'
 
 
 function AdminProducts (props){
     
-    const [product, setProduct] = useState([]
-        //products:[]
-    )
-
+    const [product, setProduct] = useState([])
+    //const [filteredProduct, setFilteredProduct] = useState([])
+    let filteredProducts = []
     useEffect(() => {
-        setProduct(Products)
+        filteredProducts = Products
+        setProduct(filteredProducts)
     }, [])
 
     const style = {
@@ -37,8 +37,26 @@ function AdminProducts (props){
     const takeToEdit = (id) => {
         history.push(`/admin/products-form/${id}`)
     }
+    const deleteProduct = (id) => {
+        alert(`product with id: ${id} deleted`)
+    }
+
+    const handleChange = (event) => {
+        //const products = filteredProducts
+        //setProduct(products)
+        let products = (event.target.value) ? filteredProducts.filter(x => x.title.toLowerCase().includes(event.target.value))
+                        : filteredProducts
+                        //debugger
+        setProduct(products)
+        //console.log(product)
+    }
+
     return (
         <>
+        <p className="col-md-4">
+        <Form.Control type="text" placeholder="Search"
+                         name="title" onChange={handleChange}/>
+        </p>
         <Button style={style} onClick={takeToPage}>
             Add New Product
         </Button>
@@ -61,6 +79,7 @@ function AdminProducts (props){
                           <td>{title}</td>
                           <td>{price}</td>
                           <td><Button onClick={() => takeToEdit(id)}>Edit</Button></td>
+                          <td><Button onClick={() => deleteProduct(id)} variant="danger">Delete</Button></td>
                        </tr>
                     )
                 })
